58baaa28b2dc833a4f452057409641673734bde9
yadha18 <myadha15@gmail.com>
2023-04-14 19:16:33 +0700

    modified at construct.php

--- construct.php
+++ construct.php
@@ -1,16 +1,24 @@
 <?php
 
-class hewan {
-    var $nama;
-    var $kaki;
+class Fruit{
+
+    private $name;
+    private $color;
+    private $price;
+
+    function __construct($name, $color, $price)
+    {
+        $this->name = $name;
+        $this->color = $color;
+        $this->price = $price;
+    }
+
+    function introduce() {
+        return $this->name ." - ". $this->color ." - ". $this->price;
+    }
 }
 
-function __construct() {
-    echo "Ini adalah isi method construct";
-}
+$apple = new Fruit("Apel", "Merah", 5000);
 
-function tampilkan_nama() {
-    return "Nama hewannya adalah ";
-}
-
+echo $apple->introduce();
 ?>
